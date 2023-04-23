05744bccc4dc371cc62089e89e1844b2c9e57000
Vũ Thành Công <104372944+vuthanhcong02@users.noreply.github.com>
2023-04-23 21:26:08 +0700

    xây dựng chức năng phân trang cho trang home

--- FE/content.php
+++ FE/content.php
@@ -1,10 +1,21 @@
 <?php
 require_once '../connectDB.php';
-$sql_A_product="SELECT * FROM products";
-$result_A_product = mysqli_query($conn,$sql_A_product);
+if (isset($_GET['page'])) {
+  $page = $_GET['page'];
+} else {
+  $page = 1;
+}
+$limit = 10;
+$offset = ($page - 1) * $limit;
+$sql_A_product = "SELECT * FROM products LIMIT $limit OFFSET $offset";
+$result_A_product = mysqli_query($conn, $sql_A_product);
+$sql_count_products = "SELECT COUNT(id) as total FROM products";
+$result_sql_count = mysqli_query($conn,$sql_count_products);
+$row_result_count = mysqli_fetch_assoc($result_sql_count);
+$total_count_products = $row_result_count['total'];
+$total_page=ceil($total_count_products/$limit);
+
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -45,13 +56,11 @@ $result_A_product = mysqli_query($conn,$sql_A_product);
   .nav li {
     border-bottom: 3px solid black;
   }
-  .card{
+
+  .card {
     margin: 10px 15px;
     background-color: white;
   }
-.card :hover{
-  /* background-color: grey; */
-}
 </style>
 
 <body>
@@ -70,7 +79,7 @@ $result_A_product = mysqli_query($conn,$sql_A_product);
   <div>
     <ul class="nav justify-content-center mb-5">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Sản phẩm mới</a>
+        <a class="nav-link" href="#">Sản phẩm mới</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Sản phẩm bán chạy</a>
@@ -80,17 +89,44 @@ $result_A_product = mysqli_query($conn,$sql_A_product);
       </li>
     </ul>
     <div class="d-flex flex-wrap justify-content-center align-items-start all-product">
-    <?php while ($row = mysqli_fetch_assoc($result_A_product)) : ?>
+      <?php while ($row = mysqli_fetch_assoc($result_A_product)) : ?>
         <div class="card p-2" style="width: 15rem;">
-        <img src="../admin/product/img/<?php echo $row['thumbnail'] ?>" class="card-img-top" alt="..."/>
+          <img src="../admin/product/img/<?php echo $row['thumbnail'] ?>" class="card-img-top" alt="..." />
           <div class="card-body">
-            <h5 class="card-title fs-6"><?php echo $row['title']?></h5>
-            <p class="card-text fs-6"><?php echo $row['price']?></p>
+            <h5 class="card-title fs-6"><?php echo $row['title'] ?></h5>
+            <p class="card-text fs-6"><?php echo $row['price'] ?></p>
             <a href="product_Infor.php?id=<?php echo $row['id'] ?>" class="btn-shop mt-3">Xem thêm</a>
           </div>
         </div>
       <?php endwhile; ?>
     </div>
+    <nav aria-label="Page navigation example" class="d-flex justify-content-center align-items-center mt-5">
+      <ul class="pagination">
+       <?php
+        if($page>1)
+         {
+          echo '<li class="page-item"><a class="page-link" href="?page='.($page-1).'" aria-label="Previous"><span aria-hidden="false">&laquo;</span></a></li>';
+         }
+
+       ?>
+        <?php
+          for($i=1;$i<=$total_page;$i++){
+            if($page==$i){
+              echo '<li class="page-item active"><a class="page-link" href="?page='.$i.'">'.$i.'</a></li>';
+            }
+            else{
+              echo '<li class="page-item"><a class="page-link" href="?page='.$i.'">'.$i.'</a></li>';
+            }
+          }
+        ?>
+        <?php
+        if($page<$total_page)
+         {
+          echo '<li class="page-item"><a class="page-link" href="?page='.($page+1).'" aria-label="Next"><span aria-hidden="false">&raquo;</span></a></li>';
+         }
+       ?>
+      </ul>
+    </nav>
   </div>
 </body>
 
