523db3b40f8c19c7f65415a6ac21574fa9170bba
Vũ Thành Công <104372944+vuthanhcong02@users.noreply.github.com>
2023-04-27 00:55:51 +0700

    demo phần tìm kiếm sản phẩm

--- FE/content.php
+++ FE/content.php
@@ -7,24 +7,38 @@ if (isset($_GET['page'])) {
 }
 $limit = 10;
 $offset = ($page - 1) * $limit;
-$sql_A_product = "SELECT * FROM products LIMIT $limit OFFSET $offset";
-$result_A_product = mysqli_query($conn, $sql_A_product);
+$search="";
+if (isset($_POST['data'])) {
+  $search = $_POST['data'];
+}
+if(!empty($search)){
+  $sql = "SELECT * FROM products WHERE title LIKE '%$search%' OR decription LIKE '%$search%'";
+}
+ else {
+  // $sql_A_product = "SELECT * FROM products LIMIT $limit OFFSET $offset";
+  // $result_A_product = mysqli_query($conn, $sql_A_product);
+  // $sql_count_products = "SELECT COUNT(id) as total FROM products";
+  // $result_sql_count = mysqli_query($conn,$sql_count_products);
+  // $row_result_count = mysqli_fetch_assoc($result_sql_count);
+  // $total_count_products = $row_result_count['total'];
+  // $total_page=ceil($total_count_products/$limit);
+  $sql = "SELECT * FROM products LIMIT $limit OFFSET $offset";
+  // $result = mysqli_query($conn, $sql);
+  // $sql_count_products = "SELECT COUNT(id) as total FROM products";
+  // $result_sql_count = mysqli_query($conn, $sql_count_products);
+  // $row_result_count = mysqli_fetch_assoc($result_sql_count);
+  // $total_count_products = $row_result_count['total'];
+  // $total_page = ceil($total_count_products / $limit);
+}
+$result = mysqli_query($conn, $sql);
+$row_num = mysqli_num_rows($result);
 $sql_count_products = "SELECT COUNT(id) as total FROM products";
-$result_sql_count = mysqli_query($conn,$sql_count_products);
+$result_sql_count = mysqli_query($conn, $sql_count_products);
 $row_result_count = mysqli_fetch_assoc($result_sql_count);
 $total_count_products = $row_result_count['total'];
-$total_page=ceil($total_count_products/$limit);
+$total_page = ceil($total_count_products / $limit);
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
 <style>
   .slogan p {
     font-size: 20px;
@@ -64,7 +78,7 @@ $total_page=ceil($total_count_products/$limit);
 </style>
 
 <body>
-  <div class=" row container mt-4 mb-5 d-flex justify-content-between align-content-center">
+  <div class="row container mt-4 mb-5 d-flex justify-content-between align-content-center">
     <div class="col-5 mt-2 slogan">
       <div class="d-flex justify-content-center flex-column align-items-end mt-5">
         <p>Fashion is the armor to survice the</p>
@@ -88,43 +102,84 @@ $total_page=ceil($total_count_products/$limit);
         <a class="nav-link" href="#">Tất cả sản phẩm</a>
       </li>
     </ul>
-    <div class="d-flex flex-wrap justify-content-center align-items-start all-product">
-      <?php while ($row = mysqli_fetch_assoc($result_A_product)) : ?>
-        <div class="card p-2" style="width: 15rem;">
-          <img src="../admin/img/<?php echo $row['thumbnail'] ?>" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title fs-6"><?php echo $row['title'] ?></h5>
-            <p class="card-text fs-6"><?php echo number_format($row['price'],0,",",".")." VND" ?></p>
-            <a href="product_Infor.php?id=<?php echo $row['id'] ?>" class="btn-shop mt-3">Xem thêm</a>
+    <section class="border d-flex flex-wrap justify-content-center align-items-start all-product search-results">
+      <?php
+      if ($row_num>0) {
+        while ($row = mysqli_fetch_assoc($result)) :
+      ?>
+          <div class="card p-2 " style="width: 15rem;">
+            <img src="../admin/img/<?php echo $row['thumbnail'] ?>" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title fs-6"><?php echo $row['title'] ?></h5>
+              <p class="card-text fs-6"><?php echo number_format($row['price'], 0, ",", ".") . " VND" ?></p>
+              <a href="product_Infor.php?id=<?php echo $row['id'] ?>" class="btn-shop mt-3">Xem thêm</a>
+            </div>
           </div>
-        </div>
-      <?php endwhile; ?>
-    </div>
-    <nav aria-label="Page navigation example" class="d-flex justify-content-center align-items-center mt-5">
-      <ul class="pagination" style="z-index:0;">
-       <?php
-        if($page>1)
-         {
-          echo '<li class="page-item"><a class="page-link" href="?page='.($page-1).'" aria-label="Previous"><span aria-hidden="false">&laquo;</span></a></li>';
-         }
-
-       ?>
+      <?php
+        endwhile;?>
+     
+      <?php
+          //  if ($page > 1) {
+          //    echo '<li class="page-item"><a class="page-link" href="?page=' . ($page - 1) . '" aria-label="Previous"><span aria-hidden="false">&laquo;</span></a></li>';
+          //  }
+          //  for ($i = 1; $i <= $total_page; $i++) {
+          //    if ($page == $i) {
+          //      echo '<li class="page-item active"><a class="page-link" href="?page=' . $i . '">' . $i . '</a></li>';
+          //    } else {
+          //      echo '<li class="page-item"><a class="page-link" href="?page=' . $i . '">' . $i . '</a></li>';
+          //    }
+          //  }
+          //  if ($page < $total_page) {
+          //    echo '<li class="page-item"><a class="page-link" href="?page=' . ($page + 1) . '" aria-label="Next"><span aria-hidden="false">&raquo;</span></a></li>';
+          //  }
+           ?>
+       
+      <?php       
+      }else{
+          echo "Không tìm thấy sản phẩm ";
+      }
+      ?>
+        <nav aria-label="Page navigation example" class="d-flex justify-content-center align-items-center mt-5">
+        <ul class="pagination" style="z-index:0;">  
         <?php
-          for($i=1;$i<=$total_page;$i++){
-            if($page==$i){
-              echo '<li class="page-item active"><a class="page-link" href="?page='.$i.'">'.$i.'</a></li>';
-            }
-            else{
-              echo '<li class="page-item"><a class="page-link" href="?page='.$i.'">'.$i.'</a></li>';
-            }
-          }
+           if ($page > 1) {
+             echo '<li class="page-item"><a class="page-link" href="?page=' . ($page - 1) . '" aria-label="Previous"><span aria-hidden="false">&laquo;</span></a></li>';
+           }
+           for ($i = 1; $i <= $total_page; $i++) {
+             if ($page == $i) {
+               echo '<li class="page-item active"><a class="page-link" href="?page=' . $i . '">' . $i . '</a></li>';
+             } else {
+               echo '<li class="page-item"><a class="page-link" href="?page=' . $i . '">' . $i . '</a></li>';
+             }
+           }
+           if ($page < $total_page) {
+             echo '<li class="page-item"><a class="page-link" href="?page=' . ($page + 1) . '" aria-label="Next"><span aria-hidden="false">&raquo;</span></a></li>';
+           }
+           ?>
+    </section>
+
+    <!-- <nav aria-label="Page navigation example" class="d-flex justify-content-center align-items-center mt-5">
+    <ul class="pagination" style="z-index:0;"> -->
+        <?php
+        // if ($page > 1) {
+        //   echo '<li class="page-item"><a class="page-link" href="?page=' . ($page - 1) . '" aria-label="Previous"><span aria-hidden="false">&laquo;</span></a></li>';
+        // }
+
+        // ?>
+       <?php
+        // for ($i = 1; $i <= $total_page; $i++) {
+        //   if ($page == $i) {
+        //     echo '<li class="page-item active"><a class="page-link" href="?page=' . $i . '">' . $i . '</a></li>';
+        //   } else {
+        //     echo '<li class="page-item"><a class="page-link" href="?page=' . $i . '">' . $i . '</a></li>';
+        //   }
+        // }
         ?>
         <?php
-        if($page<$total_page)
-         {
-          echo '<li class="page-item"><a class="page-link" href="?page='.($page+1).'" aria-label="Next"><span aria-hidden="false">&raquo;</span></a></li>';
-         }
-       ?>
+        // if ($page < $total_page) {
+        //   echo '<li class="page-item"><a class="page-link" href="?page=' . ($page + 1) . '" aria-label="Next"><span aria-hidden="false">&raquo;</span></a></li>';
+        // }
+        ?>
       </ul>
     </nav>
   </div>
--- FE/header.php
+++ FE/header.php
@@ -165,9 +165,9 @@ if (isset($_GET['category'])) {
                             ?>
                         </li>
                     </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn-shop" type="submit">Search</button>
+                    <form class="d-flex" method="post">
+                        <input class="form-control me-2 search" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn-shop" type="submit" name="search">Search</button>
                     </form>
                     <div class="d-flex justify-content-center align-content-center">
                        <a class="p-2">
@@ -220,7 +220,16 @@ if (isset($_GET['category'])) {
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript">
+        $('.search').keyup(function(){
+            var txt_search = $('.search').val();
+            $.post('content.php',{data: txt_search},function(data){
+                $('.search-results').html(data);
+            });
+        })
+    </script>
+    
 </body>
 
 </html>
